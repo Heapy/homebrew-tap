f1cd8176ae4078f129a1adcb59acaa1ca4f73236
Ruslan Ibrahimau <ruslan@heapy.io>
2026-08-14 17:27:52 +0300

    harmon 0.6.0

--- Formula/harmon.rb
+++ Formula/harmon.rb
@@ -1,8 +1,8 @@
 class Harmon < Formula
   desc "Lightweight macOS process and battery monitor"
   homepage "https://github.com/Heapy/harmon"
-  url "https://github.com/Heapy/harmon/releases/download/v0.5.0/harmon-0.5.0-macos-arm64.tar.gz"
-  sha256 "345ffce91ebdb3dad6ad85cb972c6984801fc0efda590172e0959351e7cb2793"
+  url "https://github.com/Heapy/harmon/releases/download/v0.6.0/harmon-0.6.0-macos-arm64.tar.gz"
+  sha256 "6a51fd4660ae7fc2b574bb964ffb6da23466242f2642c5dfbc81c2372f0096e7"
   license "GPL-3.0-only"
 
   depends_on arch: :arm64
@@ -13,6 +13,7 @@ class Harmon < Formula
     libexec.install "libexec/harmon-collector"
     pkgshare.install "share/harmon/Harmon.Info.plist",
                      "share/harmon/Harmon.icns",
+                     "share/harmon/PREACT-LICENSE",
                      "share/harmon/harmon.conf.example"
   end
 
